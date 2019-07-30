622ed3622c697fe7e75d77dadabcea379e519d36
hash3liZer <sheikhshameerkashif@protonmail.com>
2019-07-30 15:22:46 +0500

    Initial Execution Files

---
+++ pull.py
@@ -0,0 +1,112 @@
+import os
+import sys
+
+__logo__ = """%s
+  ______            _____              ______
+ /_/_/__/      |    | \\  \       |     | |  /
+"""
+
+class PULL:
+
+	WHITE = '\033[0m'
+	PURPLE = '\033[95m'
+	CYAN = '\033[96m'
+	DARKCYAN = '\033[36m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	END = '\033[0m'
+	LINEUP = '\033[F'
+
+	MIXTURE = {
+		'WHITE': '\033[0m',
+		'PURPLE': '\033[95m',
+		'CYAN': '\033[96m',
+		'DARKCYAN': '\033[36m',
+		'BLUE': '\033[94m',
+		'GREEN': '\033[92m',
+		'YELLOW': '\033[93m',
+		'RED': '\033[91m',
+		'BOLD': '\033[1m',
+		'UNDERLINE': '\033[4m',
+		'END': '\033[0m',
+		'LINEUP': '\033[F'
+	}
+
+	VACANT = {
+		'WHITE': '',
+		'PURPLE': '',
+		'CYAN': '',
+		'DARKCYAN': '',
+		'BLUE': '',
+		'GREEN': '',
+		'YELLOW': '',
+		'RED': '',
+		'BOLD': '',
+		'UNDERLINE': '',
+		'END': '',
+		'LINEUP': ''
+	}
+
+	def __init__(self):
+		if not self.support_colors:
+			self.win_colors()
+
+	def support_colors(self):
+		plat = sys.platform
+		supported_platform = plat != 'Pocket PC' and (plat != 'win32' or \
+														'ANSICON' in os.environ)
+		is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+		if not supported_platform or not is_a_tty:
+			return False
+		return True
+
+	def win_colors(self):
+		self.WHITE = ''
+		self.PURPLE = ''
+		self.CYAN = ''
+		self.DARKCYAN = ''
+		self.BLUE = ''
+		self.GREEN = ''
+		self.YELLOW = ''
+		self.RED = ''
+		self.BOLD = ''
+		self.UNDERLINE = ''
+		self.END = ''
+		self.MIXTURE = {
+			'WHITE': '',
+			'PURPLE': '',
+			'CYAN': '',
+			'DARKCYAN': '',
+			'BLUE': '',
+			'GREEN': '',
+			'YELLOW': '',
+			'RED': '',
+			'BOLD': '',
+			'UNDERLINE': '',
+			'END': '',
+			'LINEUP': ''
+		}
+
+		for (key, val) in self.MIXTURE.items():
+			self.MIXTURE[ key ] = ''
+
+	def gthen(self, tshow, cc='', *colors):
+		for color in colors:
+			cc += color
+		print( "%s[>]%s %s" % ( cc, self.END, tshow ) )
+
+	def lthen(self, tshow, cc='', *colors):
+		for color in colors:
+			cc += color
+		print( "%s[<]%s %s" % ( cc, self.END, tshow ) )
+
+	def halt(self, tshow, cc='', exit=1, *colors):
+		for color in colors:
+			cc += color
+		print( "%s[~]%s %s" % ( cc, self.END, tshow ) )
+		if exit:
+			sys.exit(-1)
--- snarl.py
+++ snarl.py
@@ -1,21 +1,71 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+import re
 import os
 import sys
+import socket
+import argparse
+import threading
+import subprocess
+from pull import PULL
+from web  import APP
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application 
 
+pull = PULL()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Snarl.settings')
+
+class EXECUTIONER:
+
+	def __init__(self, bd, pt):
+		self.address = bd
+		self.port    = pt
+
+	def bind(self):
+		application = get_wsgi_application()
+
+class PARSER:
+
+	def __init__(self, opts):
+		self.bind = self.bind( opts.bind )
+		self.port = self.port( opts.port )
+		self.conn = self.conn( self.bind, self.port )
+
+	def bind(self, bd):
+		if bd:
+			if re.match( r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", bd ) or re.match(r"^([a-z0-9|-]+\.)*[a-z0-9|-]+\.[a-z]+$", bd):
+				return bd
+			else:
+				pull.halt( "Not a Valid IP Address Or Domain.", pull.RED, pull.BOLD )
+		else:
+			pull.halt( "Halt! Binding Address Not Provided.", pull.RED, pull.BOLD )
+
+	def port(self, pt):
+		if pt > 0 and pt < 65536:
+			return pt
+		else:
+			pull.halt( "Invalid Port! Must be in Range 1-65535", pull.RED, pull.BOLD )
+
+	def conn(self, bd, pt):
+		try:
+			s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+			s.bind((bd, pt))
+			s.close()
+		except:
+			pull.halt( "Invalid Binding Address. Not able to Bind to: %s:%i" % (bd, pt), pull.RED, pull.BOLD )
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Snarl.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+	parser = argparse.ArgumentParser()
 
+	parser.add_argument( '-b', '--bind', dest="bind", default=None, type=str )
+	parser.add_argument( '-p', '--port', dest="port", default=8080, type=int )
 
-if __name__ == '__main__':
-    main()
+	options = parser.parse_args()
+
+	parser = PARSER( options )
+
+	pull.gthen( "Firing UP Snarl. Have a Seat! ", pull.DARKCYAN )
+	picker = EXECUTIONER( parser.bind, parser.port )
+	picker.bind()
+	pull.lthen( "Exiting!", pull.RED )
+
+if __name__ == "__main__":
+	main()
