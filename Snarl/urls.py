5a5e389a63bc08d34efcd3a5adeafa2a81eae7b9
hash3liZer <sheikhshameerkashif@protonmail.com>
2019-08-09 21:35:54 +0500

    Removed JET Dashboard Modules

--- Snarl/urls.py
+++ Snarl/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from dashboard.views import DASHBOARD
+
+dashboard = DASHBOARD()
 
 urlpatterns = [
+	path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
+    path('dashboard/', dashboard),
 ]
