99fc70d26cae472c5f520e7822ed70405c97156d
hash3liZer <sheikhshameerkashif@protonmail.com>
2019-08-15 20:30:19 +0500

    Created the slide bar.

---
+++ static/js/web.js
@@ -0,0 +1,9 @@
+// Javascript file
+// Project: Snarl
+// Authro: hash3liZer
+
+$(document).ready(function(){
+	$(".left-nav").on('click', function(){
+		$( ".slide-container" ).slideToggle( "fast" )
+	})
+})
